35b616d1ed851753c19fcca2b0173cfc8d989c6f
chenmingyong0423 <474571928@qq.com>
2023-11-08 01:30:25 +0800

    feat(aggregator): 补充测试用例

--- aggregator/aggregator_test.go
+++ aggregator/aggregator_test.go
@@ -166,3 +166,167 @@ func TestAggregator_AggregationWithOptions(t *testing.T) {
 		})
 	}
 }
+
+func TestAggregator_AggregationWithCallback(t *testing.T) {
+	type User struct {
+		Id           string `bson:"_id"`
+		Name         string `bson:"name"`
+		Age          int64
+		IsProgrammer bool `bson:"is_programmer"`
+	}
+	testCases := []struct {
+		name          string
+		mock          func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser]
+		ctx           context.Context
+		pipeline      any
+		callbackParam []*User
+		want          []*User
+		wantErr       assert.ErrorAssertionFunc
+	}{
+		{
+			name: "got error",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallback(ctx, nil, gomock.Any()).Return(errors.New("can only marshal slices and arrays into aggregation pipelines, but got invalid")).Times(1)
+				return aggregator
+			},
+			ctx:           context.Background(),
+			callbackParam: []*User{},
+			pipeline:      nil,
+			wantErr:       assert.Error,
+		},
+		{
+			name: "empty result",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallback(ctx, mongo.Pipeline{}, gomock.Any()).Return(nil).Times(1)
+				return aggregator
+			},
+			ctx:           context.Background(),
+			pipeline:      mongo.Pipeline{},
+			callbackParam: []*User{},
+			want:          []*User{},
+			wantErr:       assert.NoError,
+		},
+		{
+			name: "got result",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallback(ctx, mongo.Pipeline{
+					bson.D{
+						bson.E{Key: "$set", Value: bson.D{bson.E{Key: "is_programmer", Value: true}}},
+					},
+				}, gomock.Any()).Return(nil).Times(1)
+				return aggregator
+			},
+			ctx: context.Background(),
+			pipeline: aggregation.StageBsonBuilder().
+				Set("is_programmer", true).Build(),
+			callbackParam: []*User{
+				{Id: "1", Name: "cmy", Age: 24, IsProgrammer: true},
+			},
+			want: []*User{
+				{Id: "1", Name: "cmy", Age: 24, IsProgrammer: true},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			var callback types.ResultHandler = func(cursor *mongo.Cursor) error {
+				return cursor.All(tc.ctx, &tc.callbackParam)
+			}
+			aggregator := tc.mock(tc.ctx, ctl)
+			err := aggregator.AggregationWithCallback(tc.ctx, tc.pipeline, callback)
+			if tc.wantErr(t, err) {
+				assert.ElementsMatch(t, tc.want, tc.callbackParam)
+			}
+		})
+	}
+}
+
+func TestAggregator_AggregationWithCallbackAndOptions(t *testing.T) {
+	type User struct {
+		Id           string `bson:"_id"`
+		Name         string `bson:"name"`
+		Age          int64
+		IsProgrammer bool `bson:"is_programmer"`
+	}
+	testCases := []struct {
+		name          string
+		mock          func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser]
+		ctx           context.Context
+		pipeline      any
+		opts          *options.AggregateOptions
+		callbackParam []*User
+		want          []*User
+		wantErr       assert.ErrorAssertionFunc
+	}{
+		{
+			name: "got error",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallbackAndOptions(ctx, nil, options.Aggregate().SetBatchSize(1), gomock.Any()).Return(errors.New("can only marshal slices and arrays into aggregation pipelines, but got invalid")).Times(1)
+				return aggregator
+			},
+			ctx:           context.Background(),
+			callbackParam: []*User{},
+			opts:          options.Aggregate().SetBatchSize(1),
+			pipeline:      nil,
+			wantErr:       assert.Error,
+		},
+		{
+			name: "empty result",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallbackAndOptions(ctx, mongo.Pipeline{}, options.Aggregate().SetBatchSize(1), gomock.Any()).Return(nil).Times(1)
+				return aggregator
+			},
+			ctx:           context.Background(),
+			pipeline:      mongo.Pipeline{},
+			callbackParam: []*User{},
+			opts:          options.Aggregate().SetBatchSize(1),
+			want:          []*User{},
+			wantErr:       assert.NoError,
+		},
+		{
+			name: "got result",
+			mock: func(ctx context.Context, ctl *gomock.Controller) iAggregator[types.TestUser] {
+				aggregator := mocks.NewMockiAggregator[types.TestUser](ctl)
+				aggregator.EXPECT().AggregationWithCallbackAndOptions(ctx, mongo.Pipeline{
+					bson.D{
+						bson.E{Key: "$set", Value: bson.D{bson.E{Key: "is_programmer", Value: true}}},
+					},
+				}, options.Aggregate().SetBatchSize(1), gomock.Any()).Return(nil).Times(1)
+				return aggregator
+			},
+			opts: options.Aggregate().SetBatchSize(1),
+			ctx:  context.Background(),
+			pipeline: aggregation.StageBsonBuilder().
+				Set("is_programmer", true).Build(),
+			callbackParam: []*User{
+				{Id: "1", Name: "cmy", Age: 24, IsProgrammer: true},
+			},
+			want: []*User{
+				{Id: "1", Name: "cmy", Age: 24, IsProgrammer: true},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			var callback types.ResultHandler = func(cursor *mongo.Cursor) error {
+				return cursor.All(tc.ctx, &tc.callbackParam)
+			}
+			aggregator := tc.mock(tc.ctx, ctl)
+			err := aggregator.AggregationWithCallbackAndOptions(tc.ctx, tc.pipeline, tc.opts, callback)
+			if tc.wantErr(t, err) {
+				assert.ElementsMatch(t, tc.want, tc.callbackParam)
+			}
+		})
+	}
+}
