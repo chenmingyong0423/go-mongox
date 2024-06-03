7326b692f073f0d52a7b7685b4d7de2b3a5841a4
Mingyong Chen <chenmingyong5873@126.com>
2024-06-03 22:17:15 +0800

    refactor: Add omitempty to the bson tag for the ID field of the Model struct

--- model.go
+++ model.go
@@ -31,7 +31,7 @@ import (
 //		mongox.Model
 //	}
 type Model struct {
-	ID        primitive.ObjectID `bson:"_id"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
 	CreatedAt time.Time          `bson:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at"`
 }
