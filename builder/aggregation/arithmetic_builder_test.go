847d83c0f5828e77ac2ea236750c7ccdcafa6e85
Mingyong Chen <67659676+chenmingyong0423@users.noreply.github.com>
2024-07-03 10:13:12 +0800

    builder/aggregation: Supplement the remaining arithmetic operators (#47)
    * feat(builder/aggregation):
    - Supplement the remaining arithmetic operators.
    - Adjust the order of operator constants.

--- builder/aggregation/arithmetic_builder_test.go
+++ builder/aggregation/arithmetic_builder_test.go
@@ -235,3 +235,143 @@ func Test_arithmeticBuilder_AbsWithoutKey(t *testing.T) {
 		})
 	}
 }
+
+func Test_arithmeticBuilder_Ceil(t *testing.T) {
+	t.Run("test ceil", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "ceilingValue", Value: bson.D{bson.E{Key: "$ceil", Value: "$value"}}}},
+			NewBuilder().Ceil("ceilingValue", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_CeilWithoutKey(t *testing.T) {
+	t.Run("test ceil without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$ceil", Value: "$value"}}, NewBuilder().CeilWithoutKey("$value").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Exp(t *testing.T) {
+	t.Run("test exp", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "expValue", Value: bson.D{bson.E{Key: "$exp", Value: "$value"}}}},
+			NewBuilder().Exp("expValue", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_ExpWithoutKey(t *testing.T) {
+	t.Run("test exp without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$exp", Value: "$interestRate"}}, NewBuilder().ExpWithoutKey("$interestRate").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Floor(t *testing.T) {
+	t.Run("test floor", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "floorValue", Value: bson.D{bson.E{Key: "$floor", Value: "$value"}}}},
+			NewBuilder().Floor("floorValue", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_FloorWithoutKey(t *testing.T) {
+	t.Run("test floor without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$floor", Value: "$value"}}, NewBuilder().FloorWithoutKey("$value").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Ln(t *testing.T) {
+	t.Run("test ln", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "lnValue", Value: bson.D{bson.E{Key: "$ln", Value: "$value"}}}},
+			NewBuilder().Ln("lnValue", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_LnWithoutKey(t *testing.T) {
+	t.Run("test ln without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$ln", Value: "$value"}}, NewBuilder().LnWithoutKey("$value").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Log(t *testing.T) {
+	t.Run("test log", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "logValue", Value: bson.D{bson.E{Key: "$log", Value: bson.A{"$value", 2}}}}},
+			NewBuilder().Log("logValue", "$value", 2).Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_LogWithoutKey(t *testing.T) {
+	t.Run("test log without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$log", Value: bson.A{"$value", 2}}}, NewBuilder().LogWithoutKey("$value", 2).Build())
+	})
+}
+
+func Test_arithmeticBuilder_Log10(t *testing.T) {
+	t.Run("test log10", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "log10Value", Value: bson.D{bson.E{Key: "$log10", Value: bson.A{"$value", 10}}}}},
+			NewBuilder().Log10("log10Value", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_Log10WithoutKey(t *testing.T) {
+	t.Run("test log10 without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$log10", Value: bson.A{"$value", 10}}}, NewBuilder().Log10WithoutKey("$value").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Pow(t *testing.T) {
+	t.Run("test pow", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "powValue", Value: bson.D{bson.E{Key: "$pow", Value: bson.A{"$value", 2}}}}},
+			NewBuilder().Pow("powValue", "$value", 2).Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_PowWithoutKey(t *testing.T) {
+	t.Run("test pow without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$pow", Value: bson.A{"$value", 2}}}, NewBuilder().PowWithoutKey("$value", 2).Build())
+	})
+}
+
+func Test_arithmeticBuilder_Round(t *testing.T) {
+	t.Run("test round", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "roundValue", Value: bson.D{bson.E{Key: "$round", Value: bson.A{"$value", 2}}}}},
+			NewBuilder().Round("roundValue", "$value", 2).Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_RoundWithoutKey(t *testing.T) {
+	t.Run("test round without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$round", Value: bson.A{"$value", 2}}}, NewBuilder().RoundWithoutKey("$value", 2).Build())
+	})
+}
+
+func Test_arithmeticBuilder_Sqrt(t *testing.T) {
+	t.Run("test sqrt", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "sqrtValue", Value: bson.D{bson.E{Key: "$sqrt", Value: "$value"}}}},
+			NewBuilder().Sqrt("sqrtValue", "$value").Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_SqrtWithoutKey(t *testing.T) {
+	t.Run("test sqrt without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$sqrt", Value: "$value"}}, NewBuilder().SqrtWithoutKey("$value").Build())
+	})
+}
+
+func Test_arithmeticBuilder_Trunc(t *testing.T) {
+	t.Run("test trunc", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "truncValue", Value: bson.D{bson.E{Key: "$trunc", Value: bson.A{"$value", 0}}}}},
+			NewBuilder().Trunc("truncValue", "$value", 0).Build(),
+		)
+	})
+}
+
+func Test_arithmeticBuilder_TruncWithoutKey(t *testing.T) {
+	t.Run("test trunc without key", func(t *testing.T) {
+		assert.Equal(t, bson.D{bson.E{Key: "$trunc", Value: bson.A{"$value", 0}}}, NewBuilder().TruncWithoutKey("$value", 0).Build())
+	})
+}
